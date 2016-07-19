0ae9942093af7d64fd3a59e4e5bb3f4448effc2b
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-07-19 16:28:24 +0300

    ..

---
+++ my_first_titanic_tree.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import numpy as np
+from sklearn import tree
+
+# This line had to be added inorder for chained assignment to work properly.
+pd.options.mode.chained_assignment = None
+
+# The file train.csv is read with pandas.
+train = pd.read_csv('train.csv')
+
+# The file test.csv is read with pandas.
+test = pd.read_csv('test.csv')
+
+# The "male" and "female" values in the train table are converted to integers.
+train["Sex"][train["Sex"] == "male"] = 0
+train["Sex"][train["Sex"] == "female"] = 1
+
+# The "male" and "female" values in the train table are converted to integers.
+test["Sex"][test["Sex"] == "male"] = 0
+test["Sex"][test["Sex"] == "female"] = 1
+
+# train["Age"] = train["Age"].fillna(train["Age"].median())
+
+# # The "Embarked" classes in the train table are converted to integers.
+# train["Embarked"][train["Embarked"] == "S"] = 0
+# train["Embarked"][train["Embarked"] == "C"] = 1
+# train["Embarked"][train["Embarked"] == "Q"] = 2
+#
+# # The "Embarked" classes in the test table are converted to integers.
+# test["Embarked"][test["Embarked"] == "S"] = 0
+# test["Embarked"][test["Embarked"] == "C"] = 1
+# test["Embarked"][test["Embarked"] == "Q"] = 2
+
+# Tables for survival values and values of the features are created.
+target = train["Survived"].values
+features_one = train[["Pclass", "Sex", "Age", "Fare"]].values
+
+# The first version of the decision tree is created and fitted.
+# (Other versions of the decision tree are in different files.)
+my_tree_one = tree.DecisionTreeClassifier()
+my_tree_one = my_tree_one.fit(features_one, target)
+
+# Creates and initializes the column "Survived".
+test["Survived"] = 0
+
+# The missing values (NaN values) of the "Fare" column are replaced with median.
+test["Fare"] = test["Fare"].fillna(test["Fare"].median())
+
+# # Sets the value of "Survived" to 1 if female.
+# test["Survived"][test["Sex"] == 'female'] = 1
+
+# Creates a table with columns "PassengerId" and "Survived" from test table.
+solution = test[["PassengerId", "Survived"]]
+
+# Converts the solution table into CSV file simple_gender_classifier.csv.
+solution.to_csv('simple_gender_classifier.csv', index = False)

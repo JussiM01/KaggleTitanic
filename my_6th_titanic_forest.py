b67fba6d26f9ec4befda2cf513476b51a94e3eec
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-07-20 18:33:46 +0300

    ..

--- my_6th_titanic_forest.py
+++ my_6th_titanic_forest.py
@@ -6,6 +6,21 @@ from sklearn.ensemble import RandomForestClassifier
 # Python 3 (in Python 2 the code probably works even without it).
 pd.options.mode.chained_assignment = None
 
+decks = {
+    'A': 1,
+    'B': 2,
+    'C': 3,
+    'D': 4,
+    'E': 5,
+    'F': 6,
+    'G': 7,
+    'T': 8
+}
+
+def deck_num(cabin):
+    if isintance(cabin, str): return decks[cabin[0]]
+    return 0
+
 # The file train.csv is read with pandas.
 train = pd.read_csv('train.csv')
 
@@ -20,6 +35,10 @@ train["Sex"][train["Sex"] == "female"] = 1
 test["Sex"][test["Sex"] == "male"] = 0
 test["Sex"][test["Sex"] == "female"] = 1
 
+# A new column deck is created.
+train["Deck"] = train["Cabin"].map(deck_num)
+test["Deck"] = test["Cabin"].map(deck_num)
+
 # The missing values (NaN values) of the columns are replaced with the median.
 train["Age"] = train["Age"].fillna(train["Age"].median())
 test["Age"] = test["Age"].fillna(test["Age"].median())
@@ -31,16 +50,16 @@ test["family_size"] = test["SibSp"] + test["Parch"] + 1
 # Tables for survival values and values of the features are created.
 target = train["Survived"].values
 features_forest = train[
-        ["Sex", "family_size"]].values
+        ["Sex", "family_size", "Deck"]].values
 
 # Building and fitting the forest.
-forest = RandomForestClassifier(max_depth = 5, min_samples_split = 5,
+forest = RandomForestClassifier(max_depth = 10, min_samples_split = 2,
         n_estimators = 100, random_state = 1)
 my_forest = forest.fit(features_forest, target)
 
 # Creates a table of test features.
 test_features = test[
-        ["Sex", "family_size"]].values
+        ["Sex", "family_size", "Deck"]].values
 
 # Creates a prediction from the test features.
 pred_forest = my_forest.predict(test_features)
@@ -50,4 +69,4 @@ PassengerId = np.array(test["PassengerId"]).astype(int)
 my_solution = pd.DataFrame(pred_forest, PassengerId, columns = ["Survived"])
 
 # Solution is converted to CSV file.
-my_solution.to_csv('5th_titanic_forest.csv', index_label = ["PassengerId"])
+my_solution.to_csv('6th_titanic_forest.csv', index_label = ["PassengerId"])
